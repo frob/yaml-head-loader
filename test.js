eee698ff4d9449fa959ace7c74945c20b7d10194
Frank Anderson <frob@249517.no-reply.drupal.org>
2016-07-06 00:32:03 -0700

    added simple test coverage

--- test.js
+++ test.js
@@ -1,1 +1,21 @@
-var yml = require(index.js);
+'use strict';
+
+let fs = require('fs');
+var yml = require('./index.js');
+
+fs.readFile('./test/test.yml', 'utf-8', (err, testYAML) => {
+  if (err) {
+    console.log(err);
+  }
+  console.assert(yml(testYAML).meta.foo === 'Pellentesque Bibendum Consectetur Quam Fringilla',
+    'ERROR: testYAML frontmatter member foo'
+  );
+  console.assert(yml(testYAML).meta.bar === 'Ligula Vulputate',
+    'ERROR: testYAML frontmatter member bar'
+  );
+
+  var testTailString = 'Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.';
+  console.assert(yml(testYAML).tail === testTailString,
+    'ERROR: testYAML and the rest'
+  );
+});
